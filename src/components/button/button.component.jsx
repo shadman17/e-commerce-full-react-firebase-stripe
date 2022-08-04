db8c41e90a64905e0b51c4734c606dee0d6d30c6
Kingsman <shadman.wasif@gmail.com>
2022-08-04 16:29:23 +0600

    Added Authentication page component and sign up component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,11 +1,18 @@
-import "./button.styles.scss"
+import "./button.styles.scss";
 const BUTTON_TYPE_CLASSES = {
-    google : 'google-sign-up',
-    inverted : 'inverted',
-}
+    google: "google-sign-up",
+    inverted: "inverted",
+};
 
 const Button = ({ children, buttonType, ...otherProps }) => {
-    return <button className = {`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`} >{children}</button>;
+    return (
+        <button
+            className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+            {...otherProps}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -3,7 +3,8 @@ import {
   getAuth,
   signInWithPopup,
   GoogleAuthProvider,
-  createUserWithEmailAndPassword
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
@@ -56,4 +57,10 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
 
   return await createUserWithEmailAndPassword(auth, email, password)
+}
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password)
 }
