9808cd8d9bd730776aea2ae6ddb579c75a4fa3bf
Kingsman <shadman.wasif@gmail.com>
2022-09-29 16:48:14 +0600

    User Reducer added

--- src/contexts/user.contexts.jsx
+++ src/contexts/user.contexts.jsx
@@ -1,13 +1,45 @@
-import { createContext, useState, useEffect } from "react";
-import { onAuthStateChangedListener, signOutUser, createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+import { createContext, useState, useEffect, useReducer } from "react";
+import {
+    onAuthStateChangedListener,
+    signOutUser,
+    createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
     currentUser: null,
     setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload,
+            };
+
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`);
+    }
+};
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+    
+    const setCurrentUser = (user) => {
+        dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+    }
+
     const value = { currentUser, setCurrentUser };
     // signOutUser()
 
@@ -16,7 +48,7 @@ export const UserProvider = ({ children }) => {
             if (user) {
                 createUserDocumentFromAuth(user);
             }
-            setCurrentUser(user)
+            setCurrentUser(user);
             // console.log(user);
         });
 
@@ -27,3 +59,9 @@ export const UserProvider = ({ children }) => {
         <UserContext.Provider value={value}>{children}</UserContext.Provider>
     );
 };
+
+/*
+
+
+
+*/
